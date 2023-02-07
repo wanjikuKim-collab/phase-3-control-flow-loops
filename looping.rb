f853b7dc70fc1ffbb3ee9ac613fda9e7c3a70607
Wanjiku Kimani <wanjikukimani24@gmail.com>
2023-02-07 10:53:52 +0300

    completed lab

--- looping.rb
+++ looping.rb
@@ -1,6 +1,17 @@
+require 'pry'
+
 def happy_new_year
   # your code here
+  i = 10
+  while i>0
+    puts i  
+    i-=1
+  end
+  puts "Happy New Year!"
 end
+
+
+
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
@@ -16,9 +27,22 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  # your code her
+  (1..100).each do |i|
+    puts fizzbuzz(i)
+  end
+
 end
 
-def reverse_string(str)
-  # your code here
+
+def reverse_string(str="hello")
+  str_arr = str.split('')
+  i = str_arr.length-1
+  result_arr = []
+  while i>=0
+    result_arr.push(str_arr[i]) 
+    i -=1
+  end
+  result_arr.join('')
 end
+
